42ffd32784d2d521248dcfbd8aec0747fa66042f
gongsc <gongsc@hsfund.com>
2017-04-28 17:22:39 +0800

    first commit

---
+++ src/main/java/com/hsfund/eagle/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.hsfund.eagle.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 登录控制器
+ * @author gongsc@hsfund.com
+ */
+@Controller
+public class LoginController {
+}
---
+++ src/main/java/com/hsfund/eagle/controller/RegisterController.java
@@ -0,0 +1,8 @@
+package com.hsfund.eagle.controller;
+
+/**
+ * 注册控制器
+ * @author gongsc@hsfund.com
+ */
+public class RegisterController {
+}
